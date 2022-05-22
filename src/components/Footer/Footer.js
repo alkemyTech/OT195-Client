ec7c9b41a45119a08de334d9fc0b6c93a50cf6e4
Felipe Michalski <felimichalski@icloud.com>
2022-05-22 20:53:10 -0300

    Fetching functionality

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,10 +6,19 @@ import { Container, Row } from "react-bootstrap";
 import Brand from './Brand'
 
 const Footer = () => {
+
+    const data = { // this object must be filled by fetching information from the server
+        name: '',
+        src: ''
+    }
+
     return (
         <Container fluid className='footer-box'>
             <Row>
-                <Brand />
+                <Brand name={data.name} src={data.src} />
+            </Row>
+            <Row>
+
             </Row>
         </Container>
     )
