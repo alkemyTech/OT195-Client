5d50338153b02bd0ab9dccf2845eea49cc0daa04
Valeria Rico <valeroxanarico@gmail.com>
2022-05-23 19:58:02 -0300

    fix styles textcontact

--- src/components/ContactForm/TextContact.js
+++ src/components/ContactForm/TextContact.js
@@ -5,11 +5,7 @@ function TextContact(){
     return(
         <>
         <h1>Texto de prueba</h1>
-<<<<<<< HEAD
         <p style={{textAlign:'justify', color:'black', fontWeight:'400'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-=======
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
->>>>>>> 9af26ce9c7b8d26b3ee16c5e6c545d4cd0960374
         
         </>
 
