ddd3a44c7b7b697e9dccb7b85f8df7d0e42731a0
Maximiliano Torre <m.torre164@gmail.com>
2022-05-30 20:04:07 -0300

    Modify backoffice to render menu options according to user role

--- src/components/Backoffice/Backoffice.js
+++ src/components/Backoffice/Backoffice.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { useSelector } from "react-redux";
 import { Container, Row } from "react-bootstrap";
 import Option from "./Option";
 import Header from "./Header";
@@ -10,50 +12,66 @@ import group from "../../images/backoffice/group.png";
 import note from "../../images/backoffice/note.png";
 import organization from "../../images/backoffice/organization.png";
 import toDoList from "../../images/backoffice/to-do-list.png";
+import singleUser from "../../images/backoffice/user.png";
 
 import "./Backoffice.css";
 
 const Backoffice = () => {
+  const user = useSelector(({ user }) => user);
   const dataPlaceholder = [
     {
       title: "Novedades",
       image: newspaper,
       action: "",
+      roleId: 1,
     },
     {
       title: "Actividades",
       image: toDoList,
       action: "",
+      roleId: 1,
     },
     {
       title: "Categorias",
       image: note,
       action: "",
+      roleId: 1,
     },
     {
       title: "Testimonios",
       image: chat,
       action: "",
+      roleId: 1,
     },
     {
       title: "Organización",
       image: organization,
       action: "",
+      roleId: 1,
     },
     {
       title: "Slides",
       image: documents,
       action: "",
+      roleId: 1,
     },
     {
       title: "Usuarios",
       image: group,
       action: "",
+      roleId: 1,
     },
     {
       title: "Miembros",
       image: crowdOfUsers,
       action: "",
+      roleId: 1,
+    },
+    {
+      title: "Perfil",
+      image: singleUser,
+      action: "",
+      roleId: 2,
     },
   ];
 
@@ -63,9 +81,12 @@ const Backoffice = () => {
       <Container fluid className="bo-container d-flex">
         <Container className="bo-container__options">
           <Row className="justify-content-center">
-            {dataPlaceholder.map((el) => (
-              <Option key={el.title} option={el}></Option>
-            ))}
+            {dataPlaceholder.map(
+              (el) =>
+                user.entity.roleId === el.roleId && (
+                  <Option key={el.title} option={el}></Option>
+                )
+            )}
           </Row>
         </Container>
       </Container>
