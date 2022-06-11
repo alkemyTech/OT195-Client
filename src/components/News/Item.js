1d45c3b76e9b81229d54c4214bde24b5e4685274
Felipe Michalski <felimichalski@icloud.com>
2022-06-10 21:21:15 -0300

    Redirecting changed in news from homepage

--- src/components/News/Item.js
+++ src/components/News/Item.js
@@ -1,27 +1,26 @@
 import React from "react";
 
 import Card from "react-bootstrap/Card";
-import Button from "../Button";
+import ButtonComponent from "../Button";
 
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Item = (props) => {
   const { image, text, link } = props;
 
   const navigate = useNavigate();
-  const location = useLocation();
 
   const callbackClick = () => {
-    return navigate(location.pathname + "/" + link);
+    return navigate("/news/" + link);
   };
 
   return (
     <Card>
       <Card.Img src={"http://localhost:3005/images/" + image} />
       <Card.Text className="p-3">{text}</Card.Text>
-      <Button styles="btn-card" callbackClick={callbackClick}>
+      <ButtonComponent styles="btn-card" callbackClick={callbackClick}>
         Ver novedad
-      </Button>
+      </ButtonComponent>
     </Card>
   );
 };
