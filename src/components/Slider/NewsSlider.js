ddb4bcdd812f270aa8270dbeab81fac168316a85
Felipe Michalski <felimichalski@icloud.com>
2022-06-13 18:38:43 -0300

    Newspage dinamic info

--- src/components/Slider/NewsSlider.js
+++ src/components/Slider/NewsSlider.js
@@ -1,6 +1,8 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { AdminContext } from '../../contexts/adminContext';
 import  "./newsSlide.css";
+import { useNavigate } from "react-router-dom";
+import ButtonComponent from '../../components/Button'
 
 //Librería
 import Carousel from 'react-bootstrap/Carousel';
@@ -8,14 +10,16 @@ import Carousel from 'react-bootstrap/Carousel';
 
 const NewsSlider = () => {
   const {newsData} = useContext(AdminContext);
+
+  const navigate = useNavigate();
+
   if(newsData !== null){
-        console.log(newsData)
-        const slides= newsData.entries.map((item, i) => {
+        const slides= newsData.results.map((item, i) => {
             return (
                 <Carousel.Item key={i} interval={15000} className="carouselItemNew">
                 <img
                     className='d-block w-100'
-                    src={item.image}
+                    src={'http://localhost:3005/images/' + item.image}
                     alt={item.name}
                     style={{
                         objectFit: 'cover',
@@ -28,12 +32,13 @@ const NewsSlider = () => {
                         <h3 className='newTitle'>{item.name}</h3>
                         <p className="newContent">{"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}</p>
                     </div>
+                    <ButtonComponent className='btn-news' callbackClick={() => navigate('/news/' + item.id)}>Volver al inicio</ButtonComponent>
                 </Carousel.Caption>
                 </Carousel.Item>
                 );
             });
-        
-            return <Carousel style={{display:'block', width:'100vw'}} className='my-5 carouselNew'>{slides}</Carousel>;
+            
+            return <Carousel style={{display:'block', width:'100vw'}} className='my-5 carouselNew'>{slides}</Carousel>
     }else{
         return <></>
     }
--- src/views/News/NewsPage.jsx
+++ src/views/News/NewsPage.jsx
@@ -3,8 +3,8 @@ import Loader from "../../components/Loader/Loader";
 import {AdminContext} from '../../contexts/adminContext';
 import NewsSlider from "../../components/Slider/NewsSlider";
 import Styles from "./newsPage.module.css";
-import { useNavigate } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
+
 
 const NewsPage = () => {
 
@@ -12,44 +12,22 @@ const NewsPage = () => {
         process.env.REACT_APP_NEWS_ENDPOINT
     );
 
-    const dataExample = {
-      entries: [
-        {
-          name: "Nombre de la novedad 1",
-          image: 'https://picsum.photos/1200/500'
-        },
-        {
-          name: "Nombre de la novedad 2",
-          image: 'https://picsum.photos/1200/501'
-        },
-        {
-          name: "Nombre de la novedad 3",
-          image: 'https://picsum.photos/1202/500'
-        },
-      ],
-      ok:true
-    }
-
     const { newsData, setNewsData } = useContext(AdminContext);
-    const navigate = useNavigate()
 
     useEffect(()=>{
-      setNewsData(dataExample)
-      //Next line set db data in slides (delete previous line)
-        //setNewsData(newsInfo)
+        setNewsData(newsInfo)
     },[newsInfo, loading, setNewsData])
 
   if (loading && newsData !== null) {
     return <Loader />;
   }
+
   return (
     <main className={Styles.main}>
       <h1 className={Styles.title}>Novedades</h1>
 
       <NewsSlider/>
-      <div className={Styles.buttonContainer}>
-        <button onClick={()=>{navigate("/home")}}>Ir al inicio</button>
-      </div>
+
     </main>
   );
 };
