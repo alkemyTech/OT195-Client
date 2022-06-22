ab97d5cbe6d5c6d5dd55dfb6787f7d1f9d5b9d4d
Sergio Rivera <sergiorive02@gmail.com>
2022-06-22 19:34:15 -0300

    fix: remove console log from header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Navbar, Container } from "react-bootstrap";
 
 import NavBrand from "./NavBrand";
@@ -16,10 +16,6 @@ const Header = () => {
   );
 
   const [expandOcanvas, setExpandOcanvas] = useState(false);
-
-  useEffect(() => {
-    console.log("Rendered");
-  }, []);
 
   if (loading) {
     return <p>Loading</p>;
